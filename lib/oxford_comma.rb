f1bb6780fddd5a303ceec87c5a1d16b09893e929
Nadia Lee <miss.nadia.lee@gmail.com>
2017-12-19 20:50:41 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
   if array.length == 2
-    puts "#{array[0]} and #{array[1]}"
+    return "#{array[0]} and #{array[1]}"
   else array.length > 2
     array[-1].insert(0, "and ")
   end
